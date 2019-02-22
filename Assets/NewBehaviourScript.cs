4591f9f033f4feddf84dfe92e014b24076e9820e
chriswoo59 <46728363+chriswoo59@users.noreply.github.com>
2019-02-22 16:47:57 -0500

    cube character

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "enemy") {
+			other.gameObject.SetActive(false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/fasdfjlsd.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fasdfjlsd : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject Cone;
+
+	[SerializeField]
+	private Transform Spawn;
+
+	[SerializeField]
+	private GameObject Enemy;
+
+
+	// Use this for initialization
+	void Start () {
+		Instantiate(Enemy, Spawn, Spawn);
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < -10) {
+			transform.position = new Vector3(0,0,0);
+		}
+		if (Input.GetKey(KeyCode.W)) {
+			transform.position += new Vector3(0,0,-1);
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			transform.position += new Vector3(1,0,0);
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			transform.position += new Vector3(0,0,1);
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			transform.position += new Vector3(-1,0,0);
+		}
+		transform.Rotate(Vector3.right * Time.deltaTime);
+
+	}
+}
